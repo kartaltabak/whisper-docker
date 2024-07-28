61b1a76d4b1ae24f218d5b9c323626f5e653d821
Kartal Tabak <kartal.tabak@gmail.com>
2024-07-28 17:29:11 +0300

    create_srt

---
+++ docker/create_srt.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <input_file.mp3>"
+    exit 1
+fi
+
+input_file="$1"
+
+output_file="${input_file%.*}.srt"
+
+whisper_timestamped "${input_file}" --output_dir /tmp --output_format srt --model tiny --model_dir cache
+
+if [ $? -eq 0 ]; then
+    echo "Conversion successful: $output_file"
+else
+    echo "Conversion failed"
+    exit 1
+fi
+
+cp /tmp/*.srt ./"{output_file}"
+
